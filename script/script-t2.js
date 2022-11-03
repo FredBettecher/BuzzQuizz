9d37ca88c572915cb02a50a7147d9d72d6b75eb6
FredBettecher <fredbettecher@gmail.com>
2022-11-03 15:20:34 -0300

    pequenas modificações t3

--- script/script-t2.js
+++
@@ -1,7 +0,0 @@
-let pegarIdQuiz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-pegarIdQuiz.then(comparaIdQuiz);
-
-function comparaIdQuiz(quizzes){
-    let idQuiz = quizzes.data.id;
-    
-}
