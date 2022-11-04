1dfbb8b920c49ffe961b85c639b8429e5b759875
FredBettecher <fredbettecher@gmail.com>
2022-11-03 21:12:12 -0300

    tentativa de fazer um onclick selecionarQuizz()

--- script/script-t1.js
+++ script/script-t1.js
@@ -28,11 +28,17 @@ function renderizarTodosQuizzes(Quizzes) {
         let item = Quizzes[i];
         lista.innerHTML += `
             <li class="tamanho-imagem">
-                <div SelecionarQuiz() class="li-gradiente" style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${item.image});width: 340px;
+                <div onclick="SelecionarQuiz(this)" class="li-gradiente" style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${item.image});width: 340px;
                 height: 181px;">
                     ${item.title}
                 </div>
             </li>
         `;
     }
+}
+
+function SelecionarQuiz(){
+    console.log(item);
+    let idQuizz = item.data.id;
+    console.log(idQuizz);
 }
---
+++ script/script-t2.js
@@ -0,0 +1,23 @@
+// const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+// promise.then(resposta)
+
+// const arrayQuizz = [];
+// function resposta(res){
+//     let listaQuizz = res.data;
+//     for(let i = 0; i < listaQuizz.length; i++){
+//         arrayQuizz.push(listaQuizz[i].id);
+//     }
+//     console.log(arrayQuizz);
+// }
+
+// const idDoQuizz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/'+ quizzId);
+// idDoQuizz.then(abrirQuiz);
+
+// function SelecionarQuiz(){
+//     // clicar no quizz
+//     let idQuizz = this.data.id;
+//     console.log(idQuizz)
+//     // pegar o id do quizz clicado
+
+//     // guardar esse id numa variável
+// }
