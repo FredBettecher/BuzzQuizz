bc30b7d24a7989505eef89d6d1da98dc35bf83a6
FredBettecher <fredbettecher@gmail.com>
2022-11-05 11:56:08 -0300

    mais um commit da t2

--- script/script-t2.js
+++ script/script-t2.js
@@ -1,8 +1,10 @@
 function SelecionarQuiz(Quizzes){
     console.log(Quizzes.id);
     let id = Quizzes.id
-    let quizId = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/' + id);
-    quizId.then(respostaSelecionarQuizz);
+    const quizzPromise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/' + id);
+    quizzPromise.then(respostaSelecionarQuizz);
+    const ocultarTelaUm = document.querySelector('.e-tela1');
+    ocultarTelaUm.classList.add("ocultar");
 }
 
 function respostaSelecionarQuizz(varUnicoQuizz){
@@ -16,12 +18,22 @@ function renderizarUnicoQuizz(quizz) {
     const unicoQuizz = document.getElementById('unicoQuizz');
     unicoQuizz.innerHTML = '';
     unicoQuizz.innerHTML +=
-        `
-                <div id="${quizz.id}" onclick="SelecionarQuiz(this)" class="gradiente tamanho-imagem" 
-                    style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%,
-                    rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${quizz.image});width: 340px;
-                    height: 181px;">
-                    ${quizz.title}
-                </div>
-            `;
+        `<div class="f-gradiente" id="${quizz.id}" style="background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.57),
+            rgba(0, 0, 0, 0.57)), url(${quizz.image});">
+            ${quizz.title}
+            <div class="f-questao-um">
+            </div>
+        </div>
+        `;
+        renderizarQuestao(questaoUm);
+}
+
+function renderizarQuestao(questaoUm){
+    const questao = questaoUm.questions;
+    console.log(questao);
+    for(let i = 0; i < questao.length; i++){
+        const titulo = questao[i].titulo;
+        console.log(titulo);
+        let que
+    }
 }
